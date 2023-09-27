283e93602f8eccd8eaf84fafe96444f3190c2945
JesusGF1 <jesusgzlezferrer@gmail.com>
2023-09-26 22:42:20 -0700

    add changes to repo

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
-FROM anibali/pytorch:1.10.2-cuda11.3
+FROM anibali/pytorch:1.13.0-cuda11.8
 USER root
 
-WORKDIR ./app
+WORKDIR /src
 
 RUN sudo apt-get update
 RUN sudo apt-get install -y wget && rm -rf /var/lib/apt/lists/*
@@ -12,28 +12,9 @@ RUN sudo apt-get --allow-releaseinfo-change update && \
     sudo \
     vim
 
-RUN curl -L https://bit.ly/glances | /bin/bash
+ENV CACHE_BUST=13428901342890
 
-RUN pip install matplotlib \
-    seaborn \
-    pytorch-lightning \
-    comet_ml \
-    wandb \
-    pytorch-tabnet \
-    scanpy \
-    anndata \
-    sklearn \
-    boto3 \ 
-    tenacity \ 
-    pandas \
-    plotly \
-    scipy
-
-RUN pip install git+https://github.com/braingeneers/sims
-
-#RUN pip install streamlit==1.27.0 #It does not work :(
-RUN pip install streamlit==1.12.0 
-RUN pip install altair==4.0 
+RUN pip install -r requirements.txt
 
 EXPOSE 8501
 
--- streamlit_app.py
+++ streamlit_app.py
@@ -2,25 +2,32 @@ import scanpy as sc
 import torch
 from scsims import SIMS
 import streamlit as st
+from tempfile import NamedTemporaryFile
 import os
 
 st.write("""Upload your data here:""")
+
+uploaded_file = st.file_uploader("File upload", type='h5ad')
+with NamedTemporaryFile(dir='.', suffix='.h5ad') as f:
+    f.write(uploaded_file.getbuffer())
+    testdata = sc.read_h5ad(f.name)
+
 #file = st.file_uploader("upload file", type={"h5ad"})
+#print(file)
 #if file is not None:
-#    testdata = sc.read_h5ad(file)
+#    data_folder = file#"./"
+#    data_files = [f for f in os.listdir(data_folder) if f.endswith(".h5ad")]
+#    data_files = [os.path.join(data_folder, f) for f in data_files]
+#    selected_file = st.selectbox("Select the file you want to do label transfer on", data_files)
+#    testdata = sc.read_h5ad(selected_file)
 #else:
 #    st.write('Please upload the file which you want to predict')
 #st.write(testdata.obs)
-data_folder = "../dataset"
-data_files = [f for f in os.listdir(data_folder) if f.endswith(".h5ad")]
-data_files = [os.path.join(data_folder, f) for f in data_files]
-selected_file = st.selectbox("Select the file you want to do label transfer on", data_files)
-testdata = sc.read_h5ad(selected_file)
-
+#data_folder = "./dataset"
 
 st.write("""Select your model here:""")
 
-model_checkpoint_folder = "../checkpoint"
+model_checkpoint_folder = "./checkpoint"
 checkpoint_files = [f for f in os.listdir(model_checkpoint_folder) if f.endswith(".ckpt")]
 checkpoint_files = [os.path.join(model_checkpoint_folder, f) for f in checkpoint_files]
 selected_checkpoint = st.selectbox("Select a Model Checkpoint", checkpoint_files)
