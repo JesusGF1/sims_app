fd775d2976d19bc448daeefafbd9e521d7f8269c
JesusGF1 <jesusgzlezferrer@gmail.com>
2023-09-28 10:01:03 -0700

    working app

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,47 +5,58 @@ import streamlit as st
 from tempfile import NamedTemporaryFile
 import os
 
-st.write("""Upload your data here:""")
-
+st.write("Upload your h5ad")
 uploaded_file = st.file_uploader("File upload", type='h5ad')
-with NamedTemporaryFile(dir='.', suffix='.h5ad') as f:
-    f.write(uploaded_file.getbuffer())
-    testdata = sc.read_h5ad(f.name)
+if uploaded_file is not None:
+    with NamedTemporaryFile(dir='.', suffix='.h5ad') as f:
+        f.write(uploaded_file.getbuffer())
+        st.write(f.name)
+        testdata = sc.read_h5ad(f.name)
+        if 'testdata' not in st.session_state:
+            st.session_state['testdata'] = testdata
 
-#file = st.file_uploader("upload file", type={"h5ad"})
-#print(file)
-#if file is not None:
-#    data_folder = file#"./"
-#    data_files = [f for f in os.listdir(data_folder) if f.endswith(".h5ad")]
-#    data_files = [os.path.join(data_folder, f) for f in data_files]
-#    selected_file = st.selectbox("Select the file you want to do label transfer on", data_files)
-#    testdata = sc.read_h5ad(selected_file)
-#else:
-#    st.write('Please upload the file which you want to predict')
-#st.write(testdata.obs)
-#data_folder = "./dataset"
+if "testdata" in st.session_state:
+    st.write("""Select your model here:""")
+    model_checkpoint_folder = "./checkpoint"
+    checkpoint_files = [f for f in os.listdir(model_checkpoint_folder) if f.endswith(".ckpt")]
+    checkpoint_files = [os.path.join(model_checkpoint_folder, f) for f in checkpoint_files]
+    selected_checkpoint = st.selectbox("Select a Model Checkpoint", checkpoint_files)
+    if 'checkpoint' not in st.session_state:
+        st.session_state['checkpoint'] = selected_checkpoint
+    class_label = st.text_input("Name of the Cell Type column", "CellType") #Class label in this case I think its CellType
+    if 'label' not in st.session_state:
+        st.session_state['label'] = class_label
 
-st.write("""Select your model here:""")
+if "testdata" in st.session_state and "checkpoint" in st.session_state and "label" in st.session_state:
+    st.write("""Predict your data here:""")
+    try:
+        selected_checkpoint = st.session_state['checkpoint']
+        class_label = st.session_state['label']
+        testdata = st.session_state['testdata']
+    except:
+        st.write("Please select a model and upload your data first")
 
-model_checkpoint_folder = "./checkpoint"
-checkpoint_files = [f for f in os.listdir(model_checkpoint_folder) if f.endswith(".ckpt")]
-checkpoint_files = [os.path.join(model_checkpoint_folder, f) for f in checkpoint_files]
-selected_checkpoint = st.selectbox("Select a Model Checkpoint", checkpoint_files)
-class_label = st.text_input("Name of the Cell Type column", "CellType") #Class label in this case I think its CellType
+    if 'run' not in st.session_state:
+        st.session_state['run'] = False
+    model_run = st.session_state['run']
+    if uploaded_file is not None and selected_checkpoint is not None and class_label is not None and model_run is False:
+        sims = SIMS(weights_path=selected_checkpoint, class_label=class_label)
+        cell_predictions = sims.predict(testdata)
+        st.session_state['run'] = True
+        st.write("Predictions are done!")
+        testdata.obs = testdata.obs.reset_index()
+        testdata.obs['cell_predictions'] = cell_predictions["first_pred"]
+        testdata.obs['confidence_score'] = cell_predictions["first_prob"]
+        print(testdata.obs)
+        st.dataframe(testdata.obs)
+    else:
+        st.write("Please select a model and upload your data first")
+    
 
-st.write("""Predict your data here:""")
-if st.button('Click here to start the predictions'):
-    sims = SIMS(weights_path=selected_checkpoint, class_label=class_label)
-    cell_predictions = sims.predict(testdata)
-    st.write("Predictions are done!")
-    testdata.obs = testdata.obs.reset_index()
-    testdata.obs['cell_predictions'] = cell_predictions["first_pred"]
-    testdata.obs['confidence_score'] = cell_predictions["first_prob"]
-else:
-    st.write('Press the button to start the predictions')
+#Casi que funciona, hay que tener cuidado de no repetir los botones ya que cada interaccion reinicia el script
 
-st.write("""Visualize your data here:""")
-st.dataframe(testdata.obs)
+#st.write("""Visualize your data here:""")
+#st.dataframe(testdata.obs)
 #sc.tl.umap(testdata)
 #sc.pl.umap(testdata, color=['cell_predictions'])
 
