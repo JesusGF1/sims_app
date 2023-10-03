4489d6fc097524202ff57a2d9c625815467c7c28
JesusGF1 <jesusgzlezferrer@gmail.com>
2023-10-02 18:26:03 -0700

    Removing the label as it is no longer needed

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,18 +22,18 @@ if "testdata" in st.session_state:
     selected_checkpoint = st.selectbox("Select a Model Checkpoint", checkpoint_files)
     if 'checkpoint' not in st.session_state:
         st.session_state['checkpoint'] = selected_checkpoint
-    class_label = st.text_input("Name of the Cell Type column", "CellType") #Class label in this case I think its CellType
-    if 'label' not in st.session_state:
-        st.session_state['label'] = class_label
+    #class_label = st.text_input("Name of the Cell Type column", "CellType") #Class label in this case I think its CellType
+    #if 'label' not in st.session_state:
+    #    st.session_state['label'] = class_label
 
 
-if "testdata" in st.session_state and "checkpoint" in st.session_state and "label" in st.session_state: #Delete this if it does not work
+if "testdata" in st.session_state and "checkpoint" in st.session_state:# and "label" in st.session_state: #Delete this if it does not work
     predict = st.button("Predict your cell types")
 
-if "testdata" in st.session_state and "checkpoint" in st.session_state and "label" in st.session_state and predict:
+if "testdata" in st.session_state and "checkpoint" in st.session_state and predict: #and "label" in st.session_state
     try:
         selected_checkpoint = st.session_state['checkpoint']
-        class_label = st.session_state['label']
+        #class_label = st.session_state['label']
         testdata = st.session_state['testdata']
     except:
         st.write("Please select a model and upload your data first")
@@ -41,8 +41,8 @@ if "testdata" in st.session_state and "checkpoint" in st.session_state and "labe
     if 'run' not in st.session_state:
         st.session_state['run'] = False
     model_run = st.session_state['run']
-    if uploaded_file is not None and selected_checkpoint is not None and class_label is not None and model_run is False:
-        sims = SIMS(weights_path=selected_checkpoint, class_label=class_label)
+    if uploaded_file is not None and selected_checkpoint is not None and model_run is False:#and class_label is not None
+        sims = SIMS(weights_path=selected_checkpoint)#, class_label=class_label)
         cell_predictions = sims.predict(testdata,num_workers=0)
         st.session_state['run'] = True
         st.write("Predictions are done!")
@@ -51,7 +51,14 @@ if "testdata" in st.session_state and "checkpoint" in st.session_state and "labe
         testdata.obs['confidence_score'] = cell_predictions["first_prob"]
         print(testdata.obs)
         st.dataframe(testdata.obs)
-        st.download_button('Download predictions', testdata.obs, 'text/csv')
+        data_as_csv= testdata.obs.to_csv(index=False).encode("utf-8")
+        st.download_button(
+            "Download predictions as CSV", 
+            data_as_csv, 
+            "scSimspredictions.csv",
+            "text/csv",
+        )
+        #st.download_button('Download predictions', testdata.obs, 'text/csv')
     else:
         st.write("Please select a model and upload your data first")
     
